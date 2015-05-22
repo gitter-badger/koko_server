5cf609c7fb11b83645ef7223254d6cbcf984ca04
関　崇晃 <Takaaki.Seki@access-company.com>
2015-05-22 13:56:34 +0900

    Add test for Beacon(New) to show the list of boards by their titles

--- spec/views/beacons/new.html.erb_spec.rb
+++ spec/views/beacons/new.html.erb_spec.rb
@@ -2,20 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "beacons/new", type: :view do
   before(:each) do
-    assign(:beacon, Beacon.new(
+    assign(:board1, @board1 = Board.new(
+      :title => "Title1"
+    ))
+    assign(:board2, @board2 = Board.new(
+      :title => "Title2"
+    ))
+    assign(:beacon, @beacon = Beacon.new(
       :uuid => "MyString",
-      :board_id => 1
+      :board_id => @board2.id
     ))
   end
 
   it "renders new beacon form" do
     render
-
     assert_select "form[action=?][method=?]", beacons_path, "post" do
-
       assert_select "input#beacon_uuid[name=?]", "beacon[uuid]"
 
-      assert_select "input#beacon_board_id[name=?]", "beacon[board_id]"
+      # <select id="beacon_board_id" name="beacon[board_id]">
+      #   <option value="@board1.id">@board1.title</option>
+      #   <option value="@board2.id">@board2.title</option>
+      # </select>
+      assert_select "select#beacon_board_id" do
+        assert_select "option[value=?]", @board1.id
+        assert_select "option[value=#{@board1.id}]", :text => "Title1"
+        assert_select "option[value=?]", @board2.id
+        assert_select "option[value=#{@board2.id}]", :text => "Title2"
+      end
     end
   end
 end

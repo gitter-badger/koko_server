c73a68e9cfeb32ac9b236e1c8429726347d91d5a
関　崇晃 <Takaaki.Seki@access-company.com>
2015-05-22 14:40:26 +0900

    Add test for Beacon(Show) to show the title of the board of the beacon

--- spec/views/beacons/show.html.erb_spec.rb
+++ spec/views/beacons/show.html.erb_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "beacons/show", type: :view do
   before(:each) do
+    @board = assign(:board, Board.create!(
+      :title => "Title"
+    ))
     @beacon = assign(:beacon, Beacon.create!(
       :uuid => "Uuid",
-      :board_id => 1
+      :board_id => @board.id
     ))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Uuid/)
-    expect(rendered).to match(/1/)
+    expect(rendered).to match(/Title/)
   end
 end

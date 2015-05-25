5de623ef0a044ad8e8ec72a5d3563f2a5fea9027
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2015-05-25 10:52:50 +0900

    fixed typo in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 # Requires non-empty content and board_id specification.
 class Post < ActiveRecord::Base
   validates :content, :presence => true
-  validate :board_id, :presence => true
+  validates :board_id, :presence => true
 
   belongs_to :board, class_name: Board
 end

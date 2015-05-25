688946c6947f9abf999f10656a937e4cf1a449a2
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2015-05-25 10:50:19 +0900

    top level comment for Models. Posts now require board_id

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,9 @@
+# Model class for Boards.
+# Requires title.
+# When a board ever deleted, all associated posts will also be deleted.
 class Board < ActiveRecord::Base
+  validates :title, :presence => true
+
   has_many :beacons, class_name: Beacon
   has_many :posts, class_name: Post, dependent: :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
+# Model class for Posts.
+# Requires non-empty content and board_id specification.
 class Post < ActiveRecord::Base
+  validates :content, :presence => true
+  validate :board_id, :presence => true
+
   belongs_to :board, class_name: Board
 end

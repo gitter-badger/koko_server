c1b70f424275c5a5cc538433caf511dceb12d699
関　崇晃 <Takaaki.Seki@access-company.com>
2015-05-25 11:08:16 +0900

    Post(Show) shows the title of the board

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "posts/show", type: :view do
   before(:each) do
+    @board = assign(:board, Board.create!(
+      :title => "Test Board"
+    ))
     @post = assign(:post, Post.create!(
       :content => "MyText",
-      :board_id => 1
+      :board_id => @board.id
     ))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/1/)
+    expect(rendered).to match(/Test Board/)
   end
 end
